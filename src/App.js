6c03dadcbf4c8083bd43339d831559789e1d98d1
SanhDemo <dovansanh1211@gmail.com>
2023-01-12 09:51:32 +0700

    app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState('')
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
   return (
-    <div className="App">
-      app
+    <div className="flex gap-8 justify-center h-screen border-red-500 items-center">
+      <input
+        type='text'
+        className='outline-none border border-blue-600 px-4 w-[400px] '
+        value={value}
+        onChange={handleChange}
+      />
+      <button
+        type="button"
+        className="outline-none px-4 bg-blue-500 rounded-md text-white "
+      > add</button>
     </div>
   );
 }
